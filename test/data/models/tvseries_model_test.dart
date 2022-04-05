28e976486db5d3aa83c2413508eb0b69b5ec2e8b
dayens13 <dionsunardi1999@gmail.com>
2022-04-05 13:40:45 +0700

    finish tv series model test

---
+++ test/data/models/tvseries_model_test.dart
@@ -0,0 +1,43 @@
+
+import 'package:ditonton/data/models/tvseries_model.dart';
+import 'package:ditonton/domain/entities/tvseries.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final tTvSeriesModel = TvSeriesModel(
+    backdropPath: 'backdropPath',
+    firstAirDate: 'firstAirDate',
+    genreIds: [1, 2, 3],
+    id: 1,
+    overview: 'overview',
+    originalName: 'originalName',
+    popularity: 1,
+    posterPath: 'posterPath',
+    name: 'name',
+    voteAverage: 1,
+    voteCount: 1,
+    originalLanguage: '',
+    originCountry: [],
+  );
+
+  final tTvSeries = TvSeries(
+    backdropPath: 'backdropPath',
+    firstAirDate: 'firstAirDate',
+    genreIds: [1, 2, 3],
+    id: 1,
+    overview: 'overview',
+    originalName: 'originalName',
+    popularity: 1,
+    posterPath: 'posterPath',
+    name: 'name',
+    voteAverage: 1,
+    voteCount: 1,
+    originalLanguage: '',
+    originCountry: [],
+  );
+
+  test('should be a subclass of TvSeries entity', () async {
+    final result = tTvSeriesModel.toEntity();
+    expect(result, tTvSeries);
+  });
+}
